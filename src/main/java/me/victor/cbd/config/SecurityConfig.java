5bd9c9d4a13ef6d717c0a28002a870d10703aa4e
Victor Pavlov <ghastlydiscord@gmail.com>
2023-06-24 19:12:25 +0300

    cors enabled

--- src/main/java/me/victor/cbd/config/SecurityConfig.java
+++ src/main/java/me/victor/cbd/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package me.victor.cbd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +15,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        return http.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
